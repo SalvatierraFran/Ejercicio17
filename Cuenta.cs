545d22f6c7abb57d1e665809d3269db0256d8aee
SalvatierraFran <franco14_93@hotmail.com>
2016-04-03 21:51:31 -0300

    Programa finalizado.

---
+++ Cuenta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio17
+{
+    class Cuenta
+    {
+        private string _nombre;
+        private long _nroCuenta;
+        private double _saldo;
+        private Enumerados.ETipoInteres _tipoInteres;
+
+        public Cuenta(string Nombre, long NroCuenta, double Saldo, Enumerados.ETipoInteres TipoInteres)
+        {
+            this._nombre = Nombre;
+            this._nroCuenta = NroCuenta;
+            this._saldo = Saldo;
+            this._tipoInteres = TipoInteres;
+        }
+
+        public string getNombre()
+        {
+            return this._nombre;
+        }
+
+        public long getNroCuenta()
+        {
+            return this._nroCuenta;
+        }
+
+        public double getSaldo()
+        {
+            return this._saldo;
+        }
+
+        public Enumerados.ETipoInteres getTipoInteres()
+        {
+            return this._tipoInteres;
+        }
+
+        public void setNombre(string Nombre)
+        {
+            this._nombre = Nombre;
+        }
+
+        public void SetNroCuenta(long NroCuenta)
+        {
+            this._nroCuenta = NroCuenta;
+        }
+
+        public void setSaldo(double saldo)
+        {
+            this._saldo = saldo;
+        }
+
+        public void setTipoInteres(Enumerados.ETipoInteres tipoInteres)
+        {
+            this._tipoInteres = tipoInteres;
+        }
+    }
+}
+
+namespace Enumerados
+{
+    public enum ETipoInteres
+    {
+        TIN, TAE, TIR
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 17";
+
+            Cuenta Cuenta1 = new Cuenta("Franco", 123456, 577.50, Enumerados.ETipoInteres.TIN);
+
+            Console.Write("Nombre: " + Cuenta1.getNombre());
+            Console.Write("\nNumero de cuenta: " + Cuenta1.getNroCuenta());
+            Console.Write("\nSaldo: " + Cuenta1.getSaldo());
+            Console.Write("\nTipo de interes: " + Cuenta1.getTipoInteres());
+
+            Console.ReadKey();
+        }
+    }
+}
